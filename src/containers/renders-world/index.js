92423e80d2f09e76514de1e4e0dcf6d6f7ed9a1c
Ivan Grekov <ivan.grekov@magiclab.co>
2019-09-14 11:40:01 +0100

    move render in separate module.
    Still a lot there of code there

--- src/containers/renders-world/index.js
+++ src/containers/renders-world/index.js
@@ -1,28 +1,121 @@
 import * as THREE from 'three';
-import Ball from '../../mesh/ball/ball';
+
+import Ball, { ballRadius } from '../../mesh/ball/ball';
 import Plane from '../../mesh/ground/ground';
 import Maze from '../../mesh/maze/maze';
+
+const abs = Math.abs;
 
 class RendersWorld {
     constructor() {
         this.renderer = new THREE.WebGLRenderer();
+        this.aspect = window.innerWidth / window.innerHeight;
+
         this.setRenderSize();
     }
 
     init(maze) {
+        this.maze = maze;
+        this.scene = new THREE.Scene();
 
+        this.ballMesh = Ball.init();
+        this.scene.add(this.ballMesh);
+
+        const mazeMesh = Maze.init(this.maze);
+        this.scene.add(mazeMesh);
+
+        const planeMesh = Plane.init(this.maze.dimension);
+        this.scene.add(planeMesh);
+
+        this.camera = new THREE.PerspectiveCamera(60, this.aspect, 1, 1000);
+        this.camera.position.set(1, 1, 5);
+        this.scene.add(this.camera);
+
+        this.light = new THREE.PointLight(0xffffff, 1);
+        this.light.position.set(1, 1, 1.3);
+        this.scene.add(this.light);
+
+        this.camera.position.set(1, 1, 5);
+        this.light.position.set(1, 1, 1.3);
+
+        this.light.intensity = 0;
     }
 
-    update() {
+    update(physicsWorld) {
+        // Update ball position.
+        const stepX = physicsWorld.getPlayerShape().GetPosition().x - this.ballMesh.position.x;
+        const stepY = physicsWorld.getPlayerShape().GetPosition().y - this.ballMesh.position.y;
 
+        this.ballMesh.position.x += stepX;
+        this.ballMesh.position.y += stepY;
+
+        // Update ball rotation.
+        let tempMat = new THREE.Matrix4();
+
+        tempMat.makeRotationAxis(new THREE.Vector3(0, 1, 0), stepX / ballRadius);
+        tempMat.multiply(this.ballMesh.matrix);
+
+        this.ballMesh.matrix = tempMat;
+
+        tempMat = new THREE.Matrix4();
+        tempMat.makeRotationAxis(new THREE.Vector3(1, 0, 0), -stepY / ballRadius);
+        tempMat.multiply(this.ballMesh.matrix);
+
+        this.ballMesh.matrix = tempMat;
+        this.ballMesh.rotation.setFromRotationMatrix(this.ballMesh.matrix);
+
+        this.camera.position.x += (this.ballMesh.position.x - this.camera.position.x) * 0.1;
+        this.camera.position.y += (this.ballMesh.position.y - this.camera.position.y) * 0.1;
+        this.camera.position.z += (5 - this.camera.position.z) * 0.1;
+
+        this.light.position.x = this.camera.position.x;
+        this.light.position.y = this.camera.position.y;
+        this.light.position.z = this.camera.position.z - 3.7;
     }
 
-    show() {
+    checkVictory() {
+        const mazeX = Math.floor(this.ballMesh.position.x + 0.5);
+        const mazeY = Math.floor(this.ballMesh.position.y + 0.5);
+        const condition = mazeX == this.maze.dimension && mazeY == this.maze.dimension - 2;
+        
+        if (condition) {
+            this.maze.dimension += 2;
+        }
 
+        return condition;
     }
 
-    hide() {
+    isReady() {
+        if (abs(this.light.intensity - 1.0) < 0.05) {
+            this.light.intensity = 1.0;
 
+            return true;
+        }
+
+        return false;
+    }
+
+    isReset(reset) {
+        if (abs(this.light.intensity - 0.0) < 0.1) {
+            this.light.intensity = 0.0;
+            this.render();
+
+            return reset;
+        }
+
+        return false;
+    }
+
+    addLightIntensity() {
+        this.light.intensity += 0.1 * (1.0 - this.light.intensity);
+    }
+
+    removeLightIntensity() {
+        this.light.intensity += 0.1 * (0.0 - this.light.intensity);
+    }
+
+    getLightIntensity() {
+        return this.light.intensity;
     }
 
     render() {
@@ -31,6 +124,11 @@ class RendersWorld {
 
     setRenderSize() {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    setCameraSize() {
+        this.camera.aspect = this.aspect;
+        this.camera.updateProjectionMatrix();
     }
 
     getDomElement() {
--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import jQuery from 'jquery'; // todo: remove
-import * as THREE from 'three';
+
 import PhysicsWorld from './containers/physics-world';
 import RendersWorld from './containers/renders-world';
 
 import generateSquareMaze from './utils/maze';
 import Controls from './containers/controls/controls';
-import Ball, { ballRadius } from './mesh/ball/ball';
-import Plane from './mesh/ground/ground';
-import Maze from './mesh/maze/maze';
 
 let physicsWorld;
 let rendersWorld;
 
-let camera = undefined;
-let scene = undefined;
-let light = undefined;
 let maze = undefined;
-let mazeMesh = undefined;
 let mazeDimension = 11;
-let planeMesh = undefined;
-let ballMesh = null;
 
 let keyAxis = [0, 0];
 
@@ -27,64 +18,6 @@ let gameState = undefined;
 
 // Box2D world variables
 const step = 2;
-
-function createRenderWorld() {
-    // Create the scene object.
-    scene = new THREE.Scene();
-
-    // Add the ball.
-    ballMesh = Ball.init();
-
-    scene.add(ballMesh);
-
-    // Add the maze.
-    mazeMesh = Maze.init(maze);
-    scene.add(mazeMesh);
-
-    // Add the ground.
-    planeMesh = Plane.init(mazeDimension);
-    scene.add(planeMesh);
-
-    // Add the camera.
-    const aspect = window.innerWidth / window.innerHeight;
-
-    camera = new THREE.PerspectiveCamera(60, aspect, 1, 1000);
-    camera.position.set(1, 1, 5);
-
-    scene.add(camera);
-
-    // Add the light.
-    light = new THREE.PointLight(0xffffff, 1);
-    light.position.set(1, 1, 1.3);
-    scene.add(light);
-}
-
-function updateRenderWorld() {
-    // Update ball position.
-    const stepX = physicsWorld.getPlayerShape().GetPosition().x - ballMesh.position.x;
-    const stepY = physicsWorld.getPlayerShape().GetPosition().y - ballMesh.position.y;
-    ballMesh.position.x += stepX;
-    ballMesh.position.y += stepY;
-
-    // Update ball rotation.
-    let tempMat = new THREE.Matrix4();
-    tempMat.makeRotationAxis(new THREE.Vector3(0, 1, 0), stepX / ballRadius);
-    tempMat.multiply(ballMesh.matrix);
-    ballMesh.matrix = tempMat;
-    tempMat = new THREE.Matrix4();
-    tempMat.makeRotationAxis(new THREE.Vector3(1, 0, 0), -stepY / ballRadius);
-    tempMat.multiply(ballMesh.matrix);
-    ballMesh.matrix = tempMat;
-    ballMesh.rotation.setFromRotationMatrix(ballMesh.matrix);
-
-    // Update camera and light positions.
-    camera.position.x += (ballMesh.position.x - camera.position.x) * 0.1;
-    camera.position.y += (ballMesh.position.y - camera.position.y) * 0.1;
-    camera.position.z += (5 - camera.position.z) * 0.1;
-    light.position.x = camera.position.x;
-    light.position.y = camera.position.y;
-    light.position.z = camera.position.z - 3.7;
-}
 
 function gameLoop() {
     switch (gameState) {
@@ -94,61 +27,50 @@ function gameLoop() {
 
             physicsWorld = new PhysicsWorld(maze);
 
-            createRenderWorld();
-
-            camera.position.set(1, 1, 5);
-            light.position.set(1, 1, 1.3);
-
-            light.intensity = 0;
+            rendersWorld.init(maze);
 
             const level = Math.floor((mazeDimension - 1) / 2 - 4);
             jQuery('#level').html('Level ' + level);
 
             gameState = 'fade in';
+
             break;
 
         case 'fade in':
-            light.intensity += 0.1 * (1.0 - light.intensity);
+            rendersWorld.addLightIntensity();
+            rendersWorld.render();
 
-            rendersWorld.renderer.render(scene, camera);
+            rendersWorld.isReady();
 
-            if (Math.abs(light.intensity - 1.0) < 0.05) {
-                light.intensity = 1.0;
+            if (rendersWorld.isReady()) {
                 gameState = 'play';
             }
+
             break;
 
         case 'play':
             physicsWorld.update(keyAxis);
 
-            updateRenderWorld();
+            rendersWorld.update(physicsWorld);
+            rendersWorld.render();
 
-            rendersWorld.renderer.render(scene, camera);
-
-            // Check for victory.
-            const mazeX = Math.floor(ballMesh.position.x + 0.5);
-            const mazeY = Math.floor(ballMesh.position.y + 0.5);
-            if (mazeX == mazeDimension && mazeY == mazeDimension - 2) {
-                mazeDimension += 2;
+            if(rendersWorld.checkVictory()) {
                 gameState = 'fade out';
             }
+
             break;
 
         case 'fade out':
             physicsWorld.update(keyAxis);
 
-            updateRenderWorld();
+            rendersWorld.update(physicsWorld);
+            rendersWorld.removeLightIntensity();
+            rendersWorld.render();
 
-            light.intensity += 0.1 * (0.0 - light.intensity);
-
-            rendersWorld.renderer.render(scene, camera);
-
-            if (Math.abs(light.intensity - 0.0) < 0.1) {
-                light.intensity = 0.0;
-                rendersWorld.renderer.render(scene, camera);
-
+            if(rendersWorld.isReset(true)) {
                 gameState = 'initialize';
             }
+
             break;
     }
 
@@ -157,8 +79,7 @@ function gameLoop() {
 
 function onResize() {
     rendersWorld.setRenderSize();
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    rendersWorld.setCameraSize();
 }
 
 function onMoveKey(event) {
