8bf9a28ec9be5fc19b23e67d9d20e790826d9524
Ivan Grekov <ivan.grekov@magiclab.co>
2019-09-15 19:33:45 +0100

    add google-tracking for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-
+const HtmlReplaceWebpackPlugin = require('html-replace-webpack-plugin');
+ 
 // for analysis only
 // const { BundleStatsWebpackPlugin } = require('bundle-stats');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -8,6 +9,14 @@ const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = {
     mode: 'production',
     plugins: [
+        new HtmlWebpackPlugin(),
+        new HtmlReplaceWebpackPlugin([
+            {
+              pattern: 'your-tracking-app-ID',
+              replacement: process.env.googleID
+            },
+
+        ]),
         new CompressionPlugin(),
         // new BundleStatsWebpackPlugin(),
         // new BundleAnalyzerPlugin({
